21daf9687541128b541136b85d52f0b5771165aa
Rachel Rolon <66883810+rrolon47@users.noreply.github.com>
2021-06-11 00:28:48 +0000

    Completed part 1:  1.1c is not aligned with directions in the book, however, if you code what the book says at 1.1c it is in the correct place. Makes for added confusion.

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,11 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
 let questions;
 let correctAnswers;
 let candidateAnswers;
@@ -15,19 +15,24 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+ candidateName = input.question("What is your name?")
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+console.log(question);
+candidateAnswer = input.question("Answer: ")
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+if (candidateAnswer == correctAnswer){
+	console.log("You are correct!");
+} else {
+	console.log("Sorry, that is incorrect.");
+}
 
   let grade;
   
@@ -37,8 +42,8 @@ function gradeQuiz(candidateAnswers) {
 
 function runProgram() {
   askForName();
-  // TODO 1.1c: Ask for candidate's name //
-  
+  // TODO 1.1c: Ask for candidate's name ** this is not aligned with book ** //
+  console.log(`Welcome ${candidateName}`);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
